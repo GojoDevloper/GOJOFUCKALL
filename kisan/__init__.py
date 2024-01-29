3df910062faf20217882f96a07356125f680566d
GojoDevloper <154049785+GojoDevloper@users.noreply.github.com>
2024-01-29 11:25:40 +0530

    Update __init__.py

--- kisan/__init__.py
+++ kisan/__init__.py
@@ -2,7 +2,7 @@ import asyncio
 from pyrogram.errors.exceptions.flood_420 import FloodWait
 from pyrogram import Client,filters
 from pyrogram.types import *
-from .config import Config
+from kisan import config
 import logging
 from pyrogram.errors import (
     ChatAdminRequired
@@ -13,48 +13,8 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-if Config.STRING_SESSION:
-   ass=Client(api_id=Config.API_ID,api_hash=Config.API_HASH,session_name=Config.STRING_SESSION)   
 
-if Config.BOT_TOKEN:
-   bot=Client(":memory:",api_id=Config.API_ID,api_hash=Config.API_HASH,bot_token=Config.BOT_TOKEN)
-
-if Config.STRING_SESSION:
-  @ass.on_message(filters.command("njbanall"))
-  async def _(bot: ass, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.STRING_SESSION:
-  @ass.on_message(filters.command("mbjanall"))
-  async def mban(bot: ass, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.STRING_SESSION:
-  @ass.on_message(filters.command(["start"]))
-  async def hello(bot: ass, message):
-    await message.reply("ʜᴇʏ,{0}\n◉ɪ ᴀᴍ ᴀ ʙᴀɴᴀʟʟ ʙᴏᴛ.!\n\n\n◉ɪ ᴀᴍ ꜱᴏ ᴘᴏᴡᴇʀ ꜰᴜʟʟ ᴛᴏ ᴄʜᴇᴀᴋ ᴍʏ ᴘᴏᴡᴇʀ ɢɪʙ ᴍᴇ ꜰᴜʟʟ ᴀᴅᴍɪɴ ʀɪɢʜᴛꜱ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ ᴀɴᴅ ᴛʏᴘᴇ  /banall ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ.",)
-if Config.BOT_TOKEN:
+if config.BOT_TOKEN:
   @bot.on_message(filters.command("banall"))
   async def _(bot, msg):
     print("getting memebers from {}".format(msg.chat.id))
@@ -70,34 +30,19 @@ if Config.BOT_TOKEN:
     print("process completed")
 
 
-if Config.BOT_TOKEN:
-  @bot.on_message(filters.command("mbanall"))
-  async def mban(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.send_message(msg.chat.id, f"/ban {i.user.id}")
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print("failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
-
-
-if Config.BOT_TOKEN:
+if config.BOT_TOKEN:
   @bot.on_message(filters.command(["start"]))
   async def hello(bot, message):
-    await message.reply_photo(photo=f"https://telegra.ph/file/0dacefd89e48a69b57f2a.jpg",
-                              caption=f"ʜᴇʏ,{0}\n◉ɪ ᴀᴍ ᴀ ʙᴀɴᴀʟʟ ʙᴏᴛ.!\n\n\n◉ɪ ᴀᴍ ꜱᴏ ᴘᴏᴡᴇʀ ꜰᴜʟʟ ᴛᴏ ᴄʜᴇᴀᴋ ᴍʏ ᴘᴏᴡᴇʀ ɢɪʙ ᴍᴇ ꜰᴜʟʟ ᴀᴅᴍɪɴ ʀɪɢʜᴛꜱ ɪɴ ᴛʜɪꜱ ɢʀᴏᴜᴘ ᴀɴᴅ ᴛʏᴘᴇ  /banall ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ.",
+  await message.reply_photo(photo=f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
+                              caption=f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\ntype /banall to see magic",
 
-reply_markup=InlineKeyboardMarkup(
+  reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "𝗢𝗪𝗡𝗘𝗥", url=f"https://t.me/gojo_104")
+                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
                 ]
                 
            ]
         ),
-    )
+                           )
